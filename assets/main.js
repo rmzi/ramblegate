ee2df4cd84ce6c1991fcd106c6f611fe04d5a548
Ramzi Abdoch <rmzi@Ramzis-MacBook.local>
2020-12-18 13:43:23 -0600

    weird js

--- assets/main.js
+++ assets/main.js
@@ -1,4 +1,23 @@
-let door1 = SVG('#door_01');
+var draw = SVG('svg_container')
+
+var xhr = new XMLHttpRequest();
+xhr.open('GET', 'assets/boards/BOARD2.svg');
+xhr.send(null);
+
+xhr.onreadystatechange = function () {
+    var DONE = 4; // readyState 4 means the request is done.
+    var OK = 200; // status 200 is a successful return.
+    if (xhr.readyState === DONE) {
+      if (xhr.status === OK) {
+            console.log(xhr.responseText);
+            draw.svg(xhr.responseText)
+      } else {
+        console.log('Error: ' + xhr.status); // An error occurred during the request.
+      }
+    }
+  };
+
+
 // let door2 = SVG('#DOOR-2');
 // let door3 = SVG('#DOOR-3');
 
@@ -22,8 +41,8 @@ let unhighlight = function(){
 //     alert("Going East!");
 // }
 
-door1.on('mouseenter', highlight);
-door1.on('mouseleave', unhighlight);
+// door1.on('mouseenter', highlight);
+// door1.on('mouseleave', unhighlight);
 // door1.on('click', goWest);
 
 // door2.on('mouseenter', highlight);
