71b4b68507f518c98fc93231608f16b768d8e602
Ramzi Abdoch <rmzi@Ramzis-MacBook.local>
2020-12-18 13:37:25 -0600

    wip

--- assets/main.js
+++ assets/main.js
@@ -1,6 +1,6 @@
-let door1 = SVG('#DOOR-1');
-let door2 = SVG('#DOOR-2');
-let door3 = SVG('#DOOR-3');
+let door1 = SVG('#door_01');
+// let door2 = SVG('#DOOR-2');
+// let door3 = SVG('#DOOR-3');
 
 let highlight = function() {
     this.fill({color: '#f06'});
@@ -10,26 +10,26 @@ let unhighlight = function(){
     this.fill({color: '#ffffff'});
 }
 
-let goWest = function() {
-    alert("Going West!");
-}
+// let goWest = function() {
+//     alert("Going West!");
+// }
 
-let goNorth = function() {
-    alert("Going North!");
-}
+// let goNorth = function() {
+//     alert("Going North!");
+// }
 
-let goEast = function() {
-    alert("Going East!");
-}
+// let goEast = function() {
+//     alert("Going East!");
+// }
 
 door1.on('mouseenter', highlight);
 door1.on('mouseleave', unhighlight);
-door1.on('click', goWest);
+// door1.on('click', goWest);
 
-door2.on('mouseenter', highlight);
-door2.on('mouseleave', unhighlight);
-door2.on('click', goNorth);
+// door2.on('mouseenter', highlight);
+// door2.on('mouseleave', unhighlight);
+// door2.on('click', goNorth);
 
-door3.on('mouseenter', highlight);
-door3.on('mouseleave', unhighlight);
-door3.on('click', goEast);
+// door3.on('mouseenter', highlight);
+// door3.on('mouseleave', unhighlight);
+// door3.on('click', goEast);
